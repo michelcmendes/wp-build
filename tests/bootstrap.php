0d9e01fa8efa9276ef32d0fb36bee9595f4bd6d5
michel-mendes <michel.mendes@vwfs.com>
2024-08-25 19:23:00 +0100

    fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
 // // Load the Composer autoloader
-// require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 // // Bootstrap WordPress
-// define('WP_INSTALLING', true);
-// require_once dirname(__DIR__) . '/wordpress/wp-load.php';
+define('WP_INSTALLING', true);
+require_once dirname(__DIR__) . '/wordpress/wp-load.php';
 
 // Set up the WordPress environment
 define('WP_TESTS_DOMAIN', 'example.org');
@@ -24,14 +24,17 @@ define('WP_DEBUG', true);
 // global $wpdb;
 // $wpdb->query('CREATE TABLE wp_options (option_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT, option_name VARCHAR(191) NOT NULL DEFAULT "", option_value LONGTEXT NOT NULL, autoload VARCHAR(20) NOT NULL DEFAULT "yes", PRIMARY KEY  (option_id), UNIQUE KEY option_name (option_name))');
 
+// Load WordPress
+require dirname(__DIR__) . '/wordpress/wp-settings.php';
+
 // Load the WordPress test environment.
-$_tests_dir = getenv('WP_PHPUNIT__DIR') ?: '/tmp/wordpress-tests-lib';
+// $_tests_dir = getenv('WP_PHPUNIT__DIR') ?: '/tmp/wordpress-tests-lib';
 
-require_once $_tests_dir . '/includes/functions.php';
+// require_once $_tests_dir . '/includes/functions.php';
 
-tests_add_filter('muplugins_loaded', function() {
-    // Manually load the plugin being tested.
-    require dirname(__FILE__) . '/../wp-content/plugins/your-plugin.php';
-});
+// tests_add_filter('muplugins_loaded', function() {
+//     // Manually load the plugin being tested.
+//     require dirname(__FILE__) . '/../wp-content/plugins/your-plugin.php';
+// });
 
-require $_tests_dir . '/includes/bootstrap.php';
+// require $_tests_dir . '/includes/bootstrap.php';
